cd7c4a7eaf1f63886039e1a0151815712fddc636
Luca Capra <luca.capra@gmail.com>
2020-01-19 22:14:06 +0100

    wip rfb

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ubiquum
 
 ![remote login is not like astral projection](./ubiquum.svg)
+
+### Resources
+
+- VNC specs https://tools.ietf.org/html/rfc6143
--- rfb/server.go
+++ rfb/server.go
@@ -2,7 +2,6 @@ package rfb
 
 import (
 	"context"
-	"fmt"
 	"image"
 	"net"
 	"time"
@@ -31,7 +30,7 @@ func Serve(host string) error {
 	chClient := make(chan vnc.ServerMessage)
 
 	im := image.NewRGBA(image.Rect(0, 0, width, height))
-	tick := time.NewTicker(time.Second / 2)
+	tick := time.NewTicker(time.Second * 2)
 	defer tick.Stop()
 
 	cfg := &vnc.ServerConfig{
@@ -52,19 +51,56 @@ func Serve(host string) error {
 	// Process messages coming in on the ClientMessage channel.
 	for {
 		select {
+		case err := <-cfg.ErrorCh:
+			log.Errorf("VNC error: %s", err)
 		case <-tick.C:
+
 			err := screen.Get(im)
 			if err != nil {
 				log.Errorf("Failed to grab screen: %s", err)
 			}
-			fmt.Printf("tick\n")
+
+			// fmt.Printf("tick\n")
 		case msg := <-chClient:
 			switch msg.Type() {
 			default:
-				log.Printf("11 Received message type:%v msg:%v\n", msg.Type(), msg)
+				log.Printf("client: Received message type:%v msg:%v\n", msg.Type(), msg)
 			}
 		case msg := <-chServer:
 			switch msg.Type() {
+			case vnc.FramebufferUpdateRequestMsgType:
+
+				ev, ok := msg.(*vnc.FramebufferUpdateRequest)
+				if !ok {
+					continue
+				}
+
+				log.Infof("FramebufferUpdateRequest %dx%d [%d,%d]", ev.Width, ev.Height, ev.X, ev.Y)
+
+				// r := image.Rect(int(ev.X), int(ev.Y), int(ev.X+ev.Width), int(ev.Y+ev.Height))
+				// im1 := im.SubImage(r)
+
+				msg := new(vnc.FramebufferUpdate)
+				rawEnc := new(vnc.RawEncoding)
+				rawEnc.Colors = []vnc.Color{}
+
+				r1 := vnc.NewRectangle()
+				r1.EncType = vnc.EncRaw
+				r1.Enc = rawEnc
+
+				r1.Width = ev.Width
+				r1.Height = ev.Height
+				r1.X = ev.X
+				r1.Y = ev.Y
+
+				msg.NumRect = 1
+				msg.Rects = []*vnc.Rectangle{
+					r1,
+				}
+
+				chClient <- msg
+				log.Info("Sent response")
+
 			case vnc.PointerEventMsgType:
 				ev, ok := msg.(*vnc.PointerEvent)
 				if !ok {
@@ -78,7 +114,7 @@ func Serve(host string) error {
 				}
 				keyboard.HandleEvent(ev)
 			default:
-				log.Printf("22 Received message type:%v msg:%v\n", msg.Type(), msg)
+				log.Printf("server: Received message type:%v msg:%v\n", msg.Type(), msg)
 			}
 		}
 	}
