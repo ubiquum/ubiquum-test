4b5219438557a9bd7a1668aa9b4200483fb125b0
Luca Capra <luca.capra@gmail.com>
2020-01-21 18:38:54 +0100

    fix frame update response

--- rfb/server.go
+++ rfb/server.go
@@ -30,7 +30,7 @@ func Serve(host string) error {
 	chClient := make(chan vnc.ServerMessage)
 
 	im := image.NewRGBA(image.Rect(0, 0, width, height))
-	tick := time.NewTicker(time.Second * 2)
+	tick := time.NewTicker(time.Second / 30)
 	defer tick.Stop()
 
 	cfg := &vnc.ServerConfig{
@@ -54,18 +54,17 @@ func Serve(host string) error {
 		case err := <-cfg.ErrorCh:
 			log.Errorf("VNC error: %s", err)
 		case <-tick.C:
-
 			err := screen.Get(im)
 			if err != nil {
 				log.Errorf("Failed to grab screen: %s", err)
+				continue
 			}
-
 			// fmt.Printf("tick\n")
-		case msg := <-chClient:
-			switch msg.Type() {
-			default:
-				log.Printf("client: Received message type:%v msg:%v\n", msg.Type(), msg)
-			}
+		// case msg := <-chClient:
+		// 	switch msg.Type() {
+		// 	default:
+		// 		log.Printf("client: Received message type:%v msg:%v\n", msg.Type(), msg)
+		// 	}
 		case msg := <-chServer:
 			switch msg.Type() {
 			case vnc.FramebufferUpdateRequestMsgType:
@@ -80,25 +79,29 @@ func Serve(host string) error {
 				// r := image.Rect(int(ev.X), int(ev.Y), int(ev.X+ev.Width), int(ev.Y+ev.Height))
 				// im1 := im.SubImage(r)
 
-				msg := new(vnc.FramebufferUpdate)
-				rawEnc := new(vnc.RawEncoding)
-				rawEnc.Colors = []vnc.Color{}
-
-				r1 := vnc.NewRectangle()
-				r1.EncType = vnc.EncRaw
-				r1.Enc = rawEnc
-
-				r1.Width = ev.Width
-				r1.Height = ev.Height
-				r1.X = ev.X
-				r1.Y = ev.Y
-
-				msg.NumRect = 1
-				msg.Rects = []*vnc.Rectangle{
-					r1,
+				colors := make([]vnc.Color, 0, 0)
+				for y := 0; y < height; y++ {
+					for x := 0; x < width; x++ {
+						r, g, b, a := im.At(x, y).RGBA()
+						clr := rgbaToColor(&cfg.PixelFormat, r, g, b, a)
+						colors = append(colors, *clr)
+					}
 				}
 
-				chClient <- msg
+				cfg.ServerMessageCh <- &vnc.FramebufferUpdate{
+					NumRect: 1,
+					Rects: []*vnc.Rectangle{
+						&vnc.Rectangle{
+							X:       0,
+							Y:       0,
+							Width:   width,
+							Height:  height,
+							EncType: vnc.EncRaw,
+							Enc: &vnc.RawEncoding{
+								Colors: colors,
+							},
+						}}}
+
 				log.Info("Sent response")
 
 			case vnc.PointerEventMsgType:
@@ -118,4 +121,13 @@ func Serve(host string) error {
 			}
 		}
 	}
+}
+
+func rgbaToColor(pf *vnc.PixelFormat, r uint32, g uint32, b uint32, a uint32) *vnc.Color {
+	// fix converting rbga to rgb http://marcodiiga.github.io/rgba-to-rgb-conversion
+	clr := vnc.NewColor(pf, nil)
+	clr.R = uint16(r / 257)
+	clr.G = uint16(g / 257)
+	clr.B = uint16(b / 257)
+	return clr
 }
